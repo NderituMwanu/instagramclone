2dd92c323dfe37ce57287faf0414a9bf37b32e73
NderituMwanu <mwanugerald@gmail.com>
2020-06-01 12:09:49 +0300

    application models

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class User(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, default = "1")
@@ -20,6 +21,7 @@ class User(models.Model):
     def delete_image(self):
         self.deleter()
     
+
 
 class Profile(models.Model):
     username = models.CharField(max_length =30)
