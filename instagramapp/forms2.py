4c35be17db7fbbde39dc83b62ac8eeaf0061d0d5
NderituMwanu <mwanugerald@gmail.com>
2020-06-01 12:08:54 +0300

    registration form class

--- instagramapp/forms2.py
+++ instagramapp/forms2.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class Registration(UserCreationForm):
     email = models.EmailField()
 
